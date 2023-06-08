dbde251645adaece98d001f9a7e10aef39206306
Andrew <andrewjtrotter@gmail.com>
2023-06-09 11:50:39 +1200

    Completed api functions and loaded images

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,5 +1,28 @@
 import request from 'superagent'
+import shipImages from './data/ships'
+import { Ship, ShipData } from './models/ship'
+import { log } from 'console'
 
-export function getGreeting(): Promise<string> {
-  return request.get('/api/v1/greeting').then((res) => res.body.greeting)
+export async function fetchStarships() {
+  const data = await request.get('https://swapi.dev/api/starships')
+  // console.log(data);
+
+  const starships = data.body.results
+  // console.log(starships)
+
+  return starships as ShipData[]
+}
+
+export async function getStarships() {
+  const shipArr = await fetchStarships()
+  const updatedArr = shipArr.map(ship => {
+    return {...ship, ...shipImages.find(shipImg => shipImg.name === ship.name)}
+  })
+  
+  return updatedArr as Ship[]
+}
+
+export async function getShipById(id:number) {
+  const shipArr = await getStarships()
+  return shipArr[id]
 }
